5104f3e3b138353be1d1fde5ee7d992e25439de8
Mary <marygerina1204@gmail.com>
2019-03-17 11:10:57 +0200

    Initial Commit

---
+++ lab4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lab4
+//
+//  Created by Mary Gerina on 3/17/19.
+//  Copyright © 2019 Mary Gerina. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
